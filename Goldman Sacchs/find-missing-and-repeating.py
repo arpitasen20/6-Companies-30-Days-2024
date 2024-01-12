9ad06b03ea270b0179f9384d349a8f96a72277d1
Arpita Sen <114492179+arpitasen20@users.noreply.github.com>
2024-01-12 11:17:17 +0530

    Update find-missing-and-repeating.py

--- Goldman Sacchs/find-missing-and-repeating.py
+++ Goldman Sacchs/find-missing-and-repeating.py
@@ -7,7 +7,6 @@ class Solution:
             if(e in s):
                 r=e
             s.add(e)
-    # Aditya Seth.            
         for i in range(1,n+1):
             if((i)not in s):
                 m=i
