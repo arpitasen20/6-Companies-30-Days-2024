dcca6bb56de05d4caba84386483f5e54ee5273b7
Arpita Sen <114492179+arpitasen20@users.noreply.github.com>
2024-01-12 11:19:13 +0530

    Update find-the-winner-of-the-circular-game.js

--- Microsoft/find-the-winner-of-the-circular-game.js
+++ Microsoft/find-the-winner-of-the-circular-game.js
@@ -6,7 +6,6 @@
 var findTheWinner = function(n, k) {
     const array = Array.from({ length: n }, (_, index) => index + 1);
     var i=0;
-    //Aditya Seth.
     while (array.length > 1) {
         i = (i + k - 1) % array.length;
         array.splice(i, 1);
