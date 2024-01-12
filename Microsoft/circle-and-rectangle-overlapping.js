4225eaa174fa7f361d565f3b5c725e10d54c5c1a
Arpita Sen <114492179+arpitasen20@users.noreply.github.com>
2024-01-12 11:18:56 +0530

    Update circle-and-rectangle-overlapping.js

--- Microsoft/circle-and-rectangle-overlapping.js
+++ Microsoft/circle-and-rectangle-overlapping.js
@@ -13,6 +13,5 @@ var checkOverlap = function(radius, xCenter, yCenter, x1, y1, x2, y2) {
     const yEdge = Math.max(y1, Math.min(y2, yCenter));
     const xDistance = xCenter - xEdge;
     const yDistance = yCenter - yEdge;
-    //Aditya Seth.
     return xDistance ** 2 + yDistance ** 2 <= radius ** 2
 };
