10a65d2f1147a5174667197b7f846b5ff8023500
Arpita Sen <114492179+arpitasen20@users.noreply.github.com>
2024-01-15 11:36:24 +0530

    Create repeated-dna-sequences.py

---
+++ Microsoft/repeated-dna-sequences.py
@@ -0,0 +1,9 @@
+class Solution:
+    def findRepeatedDnaSequences(self, s: str) -> List[str]:
+        seen, res = set(), set()
+        for i in range(len(s) - 10 + 1):
+            substring = s[i:i + 10]
+            if substring in seen:
+                res.add(substring)
+            seen.add(substring)
+        return res
