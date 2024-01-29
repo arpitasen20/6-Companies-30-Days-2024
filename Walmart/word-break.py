55393261c840c7e98de7cb3b45d61d57e1ad3d7d
Arpita Sen <114492179+arpitasen20@users.noreply.github.com>
2024-01-29 20:54:21 +0530

    Create word-break.py

---
+++ Walmart/word-break.py
@@ -0,0 +1,9 @@
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        dp = [True] + [False] * len(s)
+        for i in range(1, len(s) + 1):
+            for w in wordDict:
+                if s[i - len(w): i] == w and dp[i - len(w)]:
+                    dp[i] = True
+                    break
+        return dp[-1]
